8ea66ababba1629cf345ebcca99928378a616a42
phillip-tech <pm.bogopane@gmail.com>
2024-08-12 15:47:16 +0200

    Refactor Header component and store: simplify navigation bar, update links, and modify store configuration

--- src/router.js
+++ src/router.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import ProductView from './views/ProductView.vue';
+import CartView from './views/CartView.vue';
+import FavoritesView from './views/FavoritesView.vue';
+import ComparisonListView from './views/ComparisonListView.vue';
+import Login from './views/Login.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/product/:id', name: 'ProductView', component: ProductView },
+  { path: '/cart', component: CartView, meta: { requiresAuth: true } },
+  { path: '/favorites', component: FavoritesView, meta: { requiresAuth: true } },
+  { path: '/comparison', component: ComparisonListView, meta: { requiresAuth: true } },
+  { path: '/login', component: Login },
+  // Add other routes here as necessary
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/product/:id', name: 'ProductView', component: ProductView },
-  ],
+  routes,
 });
 
 export default router;
